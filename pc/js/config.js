dd40d3442b99316b13c89c7e1c8e8faabea840d6
Real <369517224@qq.com>
2017-11-13 00:56:19 +0800

    添加弹窗css修改

--- pc/js/config.js
+++ pc/js/config.js
@@ -1,5 +1,6 @@
 var Config = {
-  debug : false,     //线上要改成false
-  url   : 'http://www.adleading.com/api/baojun730/',
+  debug : true,     //线上要改成false
+  // url   : 'http://www.adleading.com/api/baojun730/',
   // url   : 'http://localhost:81/git/www.adleading.com/api/baojun730/',
+  url   : 'http://localhost/git/www.adleading.com/api/baojun730/',
 }
